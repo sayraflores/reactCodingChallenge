730918424c75ea8c3431d813cc5a2443b7280c93
Sayra Flores <sayraflores238@gmail.com>
2022-07-15 14:25:50 -0400

    codingchallenge

---
+++ reactchallenge/src/App.js
@@ -0,0 +1,9 @@
+import {BrowserRouter as Router, Routes} from "react-router-dom"
+import Navbar from "./Navbar"
+function App () {
+  console.log(window.location)
+  return <Navbar/>
+  
+}
+
+export default App
---
+++ reactchallenge/src/Navbar.js
@@ -0,0 +1,22 @@
+export default function Navbar () {
+    return (
+    <nav className ='nav'>
+
+        <a href = "/" className="site-title">Another website</a>
+       
+       
+        <ul>
+    <li>
+        <a href ="/products">Shop</a>
+        </li>
+        <li>
+        <a href ="/contact"> Contact</a>
+    </li>
+    <li>
+        <a href ="/home">Home</a>
+        </li>
+    </ul>
+    
+    </nav>
+    )
+}
